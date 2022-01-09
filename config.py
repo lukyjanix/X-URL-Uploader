8bd88df0a5dea55cb8cbc5dbffd39c692cc88da3
lukyjanix <97372775+lukyjanix@users.noreply.github.com>
2022-01-09 14:35:26 +0530

    sada

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5072827668:AAEiarqe2_1sqOuHuWGmE_3jrqu4xLfJYVk")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 5111658)
+    APP_ID = int(os.environ.get("APP_ID", "5111658")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
